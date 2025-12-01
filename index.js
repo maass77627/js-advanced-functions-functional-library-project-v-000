037ac8418f9bf033ee75eee3048593f691a6e95d
Kelly Maass <kmaass77627@gmail.com>
2025-12-01 17:16:23 -0600

    done

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, cb) {
+      if (Array.isArray(collection)) {
+        collection.forEach((item, index) => cb(item, index, collection))
+      }
+      else {
+        for (let key in collection) {
+          cb(collection[key], key, collection)
+        }
+      }
+      return collection
 
     },
 
